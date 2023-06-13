669608b80f146bfc238589c79f990e168083cc05
Maria Pita <mahoni@bk.ru>
2023-06-13 12:30:41 +0300

    HW7 task36

--- HW7/task34.py
+++ HW7/task34.py
@@ -11,7 +11,7 @@
 # ;  “Парам пам-пам”, если с ритмом все в порядке и “Пам парам”, 
 # ;  если с ритмом все не в порядке
 
-verse = list(input("А есть ли ритм? Стих:  ").split())
+verse = list(input("А есть ли ритм? Стих:  ").lower().split())
 list_res = set()
 for word in verse:
     count = 0
@@ -25,3 +25,4 @@ if len(list_res) == 1:
 else:
     print('Пам парам')
 
+ 
